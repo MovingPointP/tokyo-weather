56600c84f2755a7b8a783593739d7953cb114633
pointp <65812086+MovingPointP@users.noreply.github.com>
2026-04-30 01:40:44 +0900

    add: weather_codeから絵文字に変換するメソッド追加

---
+++ src/lib/weatherCode.ts
@@ -0,0 +1,16 @@
+export function codeToEmoji(code: number): string {
+  if (code === 0) return "☀️";
+  if (code === 1) return "🌤️";
+  if (code === 2) return "⛅";
+  if (code === 3) return "☁️";
+  if (code === 45 || code === 48) return "🌫️";
+  if (code >= 51 && code <= 55) return "🌦️";
+  if (code === 56 || code === 57) return "🌨️";
+  if (code >= 61 && code <= 65) return "🌧️";
+  if (code === 66 || code === 67) return "🌨️";
+  if (code >= 71 && code <= 77) return "🌨️";
+  if (code >= 80 && code <= 82) return "🌧️";
+  if (code === 85 || code === 86) return "🌨️";
+  if (code >= 95) return "⛈️";
+  return "❓";
+}
