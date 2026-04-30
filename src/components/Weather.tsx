6dd4913862163d361a238aa33d9ce3c49bcfc6f0
pointp <65812086+MovingPointP@users.noreply.github.com>
2026-04-30 21:23:23 +0900

    refactor: 引数の型をpropsとして定義

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -12,6 +12,10 @@ import {
   UnknownSvg,
 } from "@/components/icons/weatherSvg";
 
+type Props = {
+  code: number;
+};
+
 const SVG_MAP: Record<string, () => React.ReactElement> = {
   sunny: SunnySvg,
   mostlySunny: MostlySunnySvg,
@@ -24,7 +28,7 @@ const SVG_MAP: Record<string, () => React.ReactElement> = {
   thunderstorm: ThunderstormSvg,
 };
 
-export default function Weather({ code }: { code: number }) {
+export default function Weather({ code }: Props) {
   const Icon = SVG_MAP[getWeatherType(code)] ?? UnknownSvg;
   return <Icon />;
 }
