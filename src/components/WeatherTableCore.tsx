65b990e2aab4fca8f79ee76eed8100739ca22719
pointp <65812086+MovingPointP@users.noreply.github.com>
2026-05-01 12:03:29 +0900

    refactor: 定義順の変更

--- src/components/WeatherTableCore.tsx
+++ src/components/WeatherTableCore.tsx
@@ -7,16 +7,16 @@ type HourlyItem = {
   weather: number;
 };
 
-const DEFAULT_DISPLAY_TIMES = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-  22, 23,
-];
-
 type Props = {
   hourly: HourlyItem[];
   displayTimes?: number[];
   className?: string;
 };
+
+const DEFAULT_DISPLAY_TIMES = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23,
+];
 
 export default function WeatherTableCore({
   hourly,
