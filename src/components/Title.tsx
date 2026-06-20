c55a2ea03a686cd1c4f1dc8d6aeea8de15c17859
pointp <65812086+MovingPointP@users.noreply.github.com>
2026-06-20 17:45:33 +0900

    update: タイトルのサイズをレスポンシブに変更

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,9 +4,9 @@ type Props = {
 
 export default function Title({ updatedAt }: Props) {
   return (
-    <div className="flex items-baseline justify-between mt-4">
-      <h1 className="text-5xl font-bold text-gray-800">本日の東京の天気</h1>
-      <span className="text-xl text-gray-600 font-bold">最終更新: {updatedAt}</span>
+    <div className="flex flex-col gap-1 sm:flex-row sm:items-baseline sm:justify-between mt-4">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-800 whitespace-nowrap">本日の東京の天気</h1>
+      <span className="text-base sm:text-lg md:text-xl text-gray-600 font-bold">最終更新: {updatedAt}</span>
     </div>
   );
 }
